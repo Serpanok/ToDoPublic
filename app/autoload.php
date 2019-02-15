9c9f0fe3b1360598407e7e7310425e6b1382c2d6
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 11:45:02 +0200

    Add autoloadMap to autoloader
    for static autoload list

--- app/autoload.php
+++ app/autoload.php
@@ -2,6 +2,15 @@
 
 class Autoload
 {
+	/**
+     * Static autoload list. Contains global classes.
+     *
+     * @var array
+     */
+	private $autoloadMap = array(
+		
+	);
+	
 	public function __construct()
 	{
 		spl_autoload_register( array($this, 'loader') );
@@ -9,6 +18,10 @@ class Autoload
 	
 	public function loader($className)
 	{
-		
+		if( isset( $this->autoloadMap[$className] ) )
+		{
+			include __DIR__ . '/' . $this->autoloadMap[$className] . '.php';
+			return;
+		}
 	}
 }
