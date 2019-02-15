2fcb000eadd5f4ecbb4412477febc3b0a46af1bc
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 13:41:41 +0200

    Create base Model class

---
+++ app/core/Model.php
@@ -0,0 +1,78 @@
+<?php
+
+abstract class Model
+{
+	/**
+     * Name of the table associated with the model
+     *
+     * @var string
+     */
+	protected static $table = null;
+	
+	/**
+     * Name of the field which is the primary key
+     *
+     * @var string
+     */
+	protected static $primaryKey = 'id';
+	
+	/**
+     * Default values of the attributes in new object
+     *
+     * @var array
+     */
+	protected static $default = [];
+	
+	/**
+     * The attributes that should be hidden from object
+     *
+     * @var array
+     */
+    protected static $hidden = [ "password" ];
+	
+	/**
+	 * @param  array  $default		Default values of attributes
+     * @return void
+     */
+	public function __construct( $default = array() )
+	{
+		$attributes = array_merge(static::$default, $default);
+		self::setAttributes($this, $attributes);
+		
+		$pk = static::$primaryKey;
+		$this->$pk = null;
+    }
+	
+	/**
+     * Set attributes in object by array.
+     *
+	 * @param  object  $object
+	 * @param  array  $attributes
+     * @return void
+     */
+	protected static function setAttributes( object &$object, array $attributes )
+	{
+		foreach($attributes as $key => $value)
+		{
+			if( array_search($key, static::$hidden) === false )
+			{
+				$object->$key = $value;
+			}
+		}
+	}
+	
+	/**
+     * Static constructor(init). Use for set static::$table by class name(ex: UserModel -> users).
+     *
+     * @return void
+     */
+	protected static function init()
+	{
+		if( static::$table === null )
+		{
+			// set the table name by Model name
+			// Ex: UserModel -> users
+			static::$table = strtolower( preg_replace('/Model$/', '', get_called_class()) ) . "s";
+		}
+	}
+}
