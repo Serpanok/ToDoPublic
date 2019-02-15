52d20dbc3425f56777a1cb0c957b3a27d3660364
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 12:44:16 +0200

    Create routes list config

---
+++ app/config/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+|	Router::get( string $route, mixed $callback ) 		- Register a new GET route
+|
+|	Router::post( string $route, mixed $callback ) 		- Register a new POST route
+|
+|	Router::redirect( string $route, string $target )	- Register a new redirect
+|
+*/
+
+Router::get("/", function(){
+	return "work!";
+});
+
+Router::get("/ping", "UserController@ping");
+
+Router::get("/user/{id}", "UserController@userPage");
+
+Router::redirect("/users", "/user/1");
--- app/core/Core.php
+++ app/core/Core.php
@@ -5,6 +5,7 @@ class Core
 	public function __construct()
 	{
 		include __DIR__ . "/../config/global.php";
+		include __DIR__ . "/../config/routes.php";
 		
 	}
 }
