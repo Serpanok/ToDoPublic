25ca2e430b4c4013eedd2b530f2c1c39767489cf
Sydorov Leonid <info@space-ship.xyz>
2019-02-16 01:27:25 +0200

    Disabled display PHP errors

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 
 require __DIR__.'/app/autoload.php';
 
 new Autoload();
 
 new Core();
+
