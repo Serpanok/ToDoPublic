57ff8268b32f90fca6e0fedaa1960e2b039f4422
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 12:48:05 +0200

    Handle request in core

--- app/core/Core.php
+++ app/core/Core.php
@@ -7,5 +7,15 @@ class Core
 		include __DIR__ . "/../config/global.php";
 		include __DIR__ . "/../config/routes.php";
 		
+		try
+		{
+			$this->request = new Request();
+
+			print Router::handle( $this->request );
+		}
+		catch(Exception $e)
+		{
+			printf("Error: %s", $e->getMessage());
+		}
 	}
 }
