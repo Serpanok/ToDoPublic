c2e886b2ee0a87168211d5cac2929737a36841e6
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 12:56:17 +0200

    Add autoload Controllers & Models

--- app/autoload.php
+++ app/autoload.php
@@ -26,5 +26,28 @@ class Autoload
 			include __DIR__ . '/' . $this->autoloadMap[$className] . '.php';
 			return;
 		}
+		
+		// parse class name by pattern
+		preg_match("/([a-zA-Z]{1,})(Controller|Model)$/", $className, $structure);
+		
+		// if its Controller or Model
+		if( isset($structure[2]) )
+		{
+			$file = null;
+			switch($structure[2])
+			{
+				case 'Controller':
+					$file = __DIR__ . "/controllers/" . $structure[0] . ".php";
+					break;
+				case 'Model':
+					$file = __DIR__ . "/models/" . $structure[0] . ".php";
+					break;
+			}
+			if( $file !== null && file_exists($file) )
+			{
+				include $file;
+			}
+			return;
+		}
 	}
 }
