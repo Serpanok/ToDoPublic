b875c54a17c551da0a9cd1576083d4fbac36c59a
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 13:10:28 +0200

    Create database config

---
+++ app/config/database.example.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Database config
+|--------------------------------------------------------------------------
+|
+|	Create /app/config/database.php by database.example.php
+|
+*/
+
+const CONFIG_DB = array(
+	'host'		=> '127.0.0.1',
+	'port'		=> '3306',
+	'database'	=> '',
+	'username'	=> '',
+	'password'	=> '',
+	'charset'	=> 'utf8mb4',
+	'prefix'	=> '',
+);
