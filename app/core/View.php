489be188e82e084f0ca11dc787f8ea374a00277a
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 16:39:46 +0200

    Add View::render method

--- app/core/View.php
+++ app/core/View.php
@@ -2,6 +2,31 @@
 
 class View
 {
-	
+	/**
+	 * Renver and return view with attributes.
+	 *
+	 * @param  string  $view
+	 * @param  array  $attributes
+	 * @param  array  $settings
+	 * @return string
+	 */
+	public static function render( $view, array $attributes = array(), $settings = array() )
+	{
+		$smarty = new Smarty;
+		$smarty->template_dir = __DIR__ . '/../views';
+		$smarty->compile_dir = __DIR__ . '/../views/compile';
+		
+		foreach( $attributes as $key => $value )
+		{
+			$smarty->assign($key, $value);
+		}
+		
+		foreach( $settings as $attr => $value )
+		{
+			$smarty->$attr = $value;
+		}
+		
+		return $smarty->fetch($view . ".tpl");
+	}
 }
 
