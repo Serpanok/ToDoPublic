58c77d80f06e18319ac714eb31197694052b2d63
Sydorov Leonid <info@space-ship.xyz>
2019-02-15 18:09:24 +0200

    Create View::main method

--- app/core/View.php
+++ app/core/View.php
@@ -3,7 +3,7 @@
 class View
 {
 	/**
-	 * Renver and return view with attributes.
+	 * Render and return view with attributes.
 	 *
 	 * @param  string  $view
 	 * @param  array  $attributes
@@ -11,6 +11,49 @@ class View
 	 * @return string
 	 */
 	public static function render( $view, array $attributes = array(), $settings = array() )
+	{
+		$smarty = self::prepareSmarty( $attributes, $settings );
+		
+		return $smarty->fetch($view . ".tpl");
+	}
+	
+	/**
+     * Name of main teamplate
+     *
+     * @var string
+     */
+	public static $mainTpl = "main";
+	
+	/**
+	 * Render and return view with main teamplate.
+	 *
+	 * @param  string  $content
+	 * @param  array  $attributes
+	 * @param  array  $settings
+	 * @return string
+	 */
+	public static function main( $content, array $attributes = array(), $settings = array() )
+	{
+		if( static::$mainTpl === null )
+		{
+			return $content;
+		}
+		
+		$smarty = self::prepareSmarty( $attributes, $settings );
+		
+		$smarty->assign("content", $content);
+		
+		return $smarty->fetch(static::$mainTpl . ".tpl");
+	}
+	
+	/**
+	 * Return prepeared Smarty object
+	 *
+	 * @param  array  $attributes
+	 * @param  array  $settings
+	 * @return Smarty
+	 */
+	protected static function prepareSmarty( array $attributes = array(), $settings = array() )
 	{
 		$smarty = new Smarty;
 		$smarty->template_dir = __DIR__ . '/../views';
@@ -26,7 +69,7 @@ class View
 			$smarty->$attr = $value;
 		}
 		
-		return $smarty->fetch($view . ".tpl");
+		return $smarty;
 	}
 }
 
